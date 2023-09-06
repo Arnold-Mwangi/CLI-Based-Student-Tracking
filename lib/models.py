bbfc33aa8e4f313554bc855d93a2a9db8eaf1cc4
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-09-06 23:44:01 +0300

    Seeded data for Student, Teacher and Subject model

--- lib/models.py
+++ lib/models.py
@@ -1,8 +1,9 @@
 from sqlalchemy import String, Integer, PrimaryKeyConstraint,CheckConstraint, Boolean, ForeignKeyConstraint, ForeignKey, MetaData, Sequence,Column, text
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, UniqueConstraint, Table
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -18,6 +19,13 @@ Session = sessionmaker(bind =  engine)
 
 session = Session()
 
+student_subject_association = Table(
+    'student_subject_association',
+    Base.metadata,
+    Column('student_id', Integer(), ForeignKey('students.student_id')),
+    Column('subject_id', Integer, ForeignKey('subjects.id'))
+)
+
 class Student(Base):
     __tablename__ = "students"
 
@@ -28,13 +36,29 @@ class Student(Base):
     gender = Column(String())
     phone_number = Column(String())
 
+    teachers = relationship('Teacher', back_populates='student')
+    subjects = relationship('Subject', secondary=student_subject_association, back_populates='students')
 
 
 class Subject(Base):
     __tablename__ = "subjects"
+    __table_args__ = (UniqueConstraint('subject_code', name='uq_subject_code'),)
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String())
     subject_code = Column(String())
 
+    teachers = relationship('Teacher', back_populates='subject')
+    students =relationship('Student', secondary=student_subject_association, back_populates='subjects')
 
+
+class Teacher(Base):
+    __tablename__ = "teachers"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    student_id = Column(Integer, ForeignKey('students.student_id'))
+    subject_id = Column(String, ForeignKey('subjects.id'))
+
+    student = relationship('Student', back_populates='teachers')
+    subject=relationship('Subject', back_populates='teachers')
--- lib/seed.py
+++ lib/seed.py
@@ -15,12 +15,15 @@ project_root = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.append(project_root)
 
 # Now you can import modules from the 'lib' package (not 'db')
-from lib.models import Student, Subject, Base, session
+from lib.models import Student, Subject, Base, session, Teacher
 
 
 if __name__ == '__main__':
     session.query(Student).delete()
     session.query(Subject).delete()
+    session.query(Teacher).delete()
+    
+
 
     fake = Faker()
 
@@ -53,14 +56,66 @@ if __name__ == '__main__':
         "Big Data and Hadoop": "BDH808"
     }
 
+    subjects = []
+    used_subject_codes = set()
 
-    subjects=[]
-    for x in range (8):
-        course_name, subject_code = random.choice(list(technical_courses.items()))
-        subject=Subject(
-            name= course_name,
-            subject_code= subject_code
+    for _ in range(8):
+        available_courses = [course for course in technical_courses.items() if course[1] not in used_subject_codes]
+        if not available_courses:
+            break  # No more unique subject codes available
+        course_name, subject_code = random.choice(available_courses)
+        used_subject_codes.add(subject_code)
+        
+        subject = Subject(
+            name=course_name,
+            subject_code=subject_code
         )
         subjects.append(subject)
         session.add(subject)
+
     session.commit()
+
+teacher_names = ["Alice Smith", "John Johnson", "Emily Davis", "Michael Wilson"]
+
+teachers = []
+# Create a shuffled list of student IDs
+shuffled_student_ids = [student.student_id for student in students]
+random.shuffle(shuffled_student_ids)
+
+# Create a list of associations
+associations = []
+
+# Create instances of Teacher with shuffled student IDs and random subjects
+for teacher_name in teacher_names:
+    for x in range(len(students)):
+        if not shuffled_student_ids:  # If the list is empty, shuffle it again
+            shuffled_student_ids = [student.student_id for student in students]
+            random.shuffle(shuffled_student_ids)
+        
+        # Pop a student ID from the shuffled list
+        student_id = shuffled_student_ids.pop()
+
+        # Randomly select a subject from the existing records
+        subject = random.choice(subjects)
+        
+        teacher = Teacher(
+            name=random.choice(teacher_names), 
+            student_id=student_id, 
+            subject_id=subject.id)
+            
+        session.add(teacher)
+        associations.append((teacher, student_id, subject))
+
+# Commit the session to save the teachers to the database
+try:
+    session.commit()
+    print("Teachers seeded successfully.")
+    for teacher, student_id, subject in associations:
+        print(f"Teacher: {teacher.name}, Student ID: {student_id}, Subject: {subject.name}")
+except Exception as e:
+    session.rollback()
+    print(f"Error during session commit: {str(e)}")
+
+
+
+
